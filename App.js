80296e32aca9fad5d98dff92afcaddfbabe74962
jiangchengweijcw@qq.com <jiangchengweijcw@qq.com>
2019-04-10 16:04:46 +0800

    重新规划文件结构

--- App.js
+++ App.js
@@ -8,14 +8,12 @@
 
 import React, {Component} from 'react'
 import {Platform, StyleSheet, Text, View} from 'react-native'
-import Screen from './src/components/Screen'
-import Formulae from "./src/components/Formulae"
-import Keyboard from "./src/components/Keyboard";
 
 export default class App extends Component {
+
   render() {
     return (
-      <View class="container">
+      <View style={styles.container}>
         <Screen />
         <Formulae />
         <Keyboard />
@@ -28,7 +26,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: "flex-end",
+    backgroundColor: '#000000',
   }
 });
